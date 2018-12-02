585eb93f7e3628461f6145ed0e1fa5f1398e28ee
Mohammed Boudad <mboudad7@gmail.com>
2018-12-02 16:26:30 +0100

    Add index.js to windowLayouts

---
+++ src/components/windowLayouts/index.js
@@ -0,0 +1,7 @@
+import AddStudent from "./AddStudent";
+import Students from "./Students";
+import Settings from "./Settings";
+import About from "./About";
+import EditStudent from "./EditStudent";
+
+export { AddStudent, Students, Settings, About, EditStudent };
